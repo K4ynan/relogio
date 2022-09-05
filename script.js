49ca2283c2c3911cc9cd301759dd9f0746f5052f
K4ynan <kaynancamilo12@gmail.com>
2022-09-05 17:18:13 -0300

    relogio

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
+const milesimos = document.getElementById('milesimos');
+const dias = document.getElementById('dias');
+const mês = document.getElementById('mês');
+const ano = document.getElementById('ano');
 
 const relogio = setInterval(function time() {
     let dateToday = new Date();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let s = dateToday.getSeconds();
+    let mill = dateToday.getMilliseconds();
+    let day = dateToday.getDate();
+    let calendar = dateToday.getMonth() + 1;
+    let year = dateToday.getFullYear();
 
     if (hr < 10) hr = '0' + hr;
 
@@ -14,8 +22,23 @@ const relogio = setInterval(function time() {
 
     if (s < 10) s = '0' + s;
 
+    if (mill < 10) mill = '0' + mill;
+
+    if (day < 10) day = '0' + day;
+
+    if (calendar < 10) calendar = '0' + calendar;
+
+    if (year < 10) year = '0' + year;
+
+
+
+
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = s;
+    milesimos.textContent = mill;
+    dias.textContent = day;
+    mês.textContent = calendar;
+    ano.textContent = year;
 
 })
